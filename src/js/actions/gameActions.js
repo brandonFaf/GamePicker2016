f5fec94c1c528652ae651f59d06d541843a76d55
Brandon Myers <brandon.meyers@rockfishinteractive.com>
2016-08-20 13:54:57 -0400

    added ids to games when fetched so they can show in pick screen

--- src/js/actions/gameActions.js
+++ src/js/actions/gameActions.js
@@ -3,20 +3,16 @@ import GameAPI from '../../data/GameAPI';
 import firebase from 'firebase';
 
 function loadGamesSuccess(games) {
-  //TODO: return only the current week and week before and after
-  console.log("here");
   return {type:types.LOAD_GAMES_SUCCESS, games}
 }
 
 function savePickSuccess(game) {
-  //TODO: return only the current week and week before and after
-  console.log("here");
   return {type:types.SAVE_PICK, game}
 }
 
 export function savePick(game) {
   return function (dispatch) {
-    return GameAPI.savePick(game).then( (game) => {
+    return firebase.datbase().ref(`games/${game.index}`).set(game).then( (game) => {
       dispatch(savePickSuccess(game))
     }).catch( (err) => {
       throw err;
@@ -26,8 +22,15 @@ export function savePick(game) {
 
 export function loadGames(){
   return function (dispatch) {
-    return firebase.database().ref('games').once('value').then((games) => {
-      dispatch(loadGamesSuccess(games.val()))
+    return firebase.database().ref('games').once('value').then((snapshot) => {
+      //TODO: return only the current week and week before and after and get the other weeks later
+      //A hacky way to do this would be to grab like 16*3 games around the current week. I'll grab extras though because of byes
+      let keys = Object.keys(snapshot.val());
+      //give the game an id based on the key it has. Have to do it with keys because might not always get an array back.
+      let games = snapshot.val().map((n,i) => {
+        return Object.assign(n,{id: keys[i]});
+      })
+      dispatch(loadGamesSuccess(games))
     }).catch( (err) => {
       throw(err);
     })
